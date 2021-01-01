63d26a8b4110a148f08d8f9899ee51d29f4119d4
Chris White <cxwembedded@gmail.com>
2021-01-01 15:01:56 -0500

    Keep Asset::made_by as a weak reference

--- lib/App/hopen/Asset.pm
+++ lib/App/hopen/Asset.pm
@@ -4,7 +4,8 @@ use strict; use warnings;
 use Data::Hopen::Base;
 
 use Path::Class;
-# and we use Class::Tiny below.
+use Scalar::Util qw(weaken);
+# and we use Class::Tiny below.  This class has no parent.
 
 our $VERSION = '0.000013'; # TRIAL
 
@@ -50,8 +51,10 @@ No default, so don't call C<< $obj->target >> until you've assigned a target!
 
 =head2 made_by
 
-The L<App::hopen::G::Cmd> (or subclass) instance that produced this asset.
-Used to distinguish assets from different sources.
+The L<App::hopen::G::Cmd> (or subclass) instance that produced this asset, or
+C<undef>.  Used to distinguish assets from different sources.  Stored as a weak
+reference, but will probably be returned as a strong reference --- see
+L<Scalar::Util/weaken>.
 
 =head2 name
 
@@ -85,10 +88,24 @@ sub target {
     }
 } #target()
 
-# Create the accessor that enforces the restriction on made_by
-use Class::Tiny::ConstrainedAccessor [NOBUILD => true],
-    made_by => [ sub { eval { $_[0]->DOES('App::hopen::G::Cmd') } },
-                sub { 'made_by values must implement App::hopen::G::Cmd' } ];
+# The accessor for the made_by attribute.  Makes sure values are weak refs.
+sub made_by {
+    my $self = shift;
+    if (@_) {   # Setter
+        my $candidate = shift;
+
+        return $self->{made_by} = $candidate unless defined $candidate;
+        croak 'made_by values must implement App::hopen::G::Cmd'
+            unless eval { $_[0]->DOES('App::hopen::G::Cmd') };
+
+        $self->{made_by} = $candidate;
+        weaken $self->{made_by};
+        return $self->{made_by};    # NOTE: most likely a strong ref
+
+    } else {    # Getter
+        return $self->{made_by};
+    }
+} #made_by()
 
 # Set up the rest of the class
 use Class::Tiny qw(target made_by name);
@@ -119,9 +136,9 @@ my $_id_counter;
 sub BUILD {
     my ($self) = @_;
     $self->name('__R_Asset_' . $_id_counter++) unless $self->name;
+    # Check the custom constraints by re-setting the values
     $self->target($self->{target});     # Check the custom constraint on target
-                                        # by re-setting target.
-    $self->_check_all_constraints();    # Checks made_by.
+    $self->made_by($self->{made_by});
 } #BUILD()
 
 1;
